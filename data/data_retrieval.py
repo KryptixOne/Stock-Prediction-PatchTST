2406b63bdc74d39d890e89d5bc8aa354229f21d9
Daniel Caraballo <danielcaraballo@daniels-mbp.speedport.ip>
2024-08-21 13:42:24 +0200

    Feat: Technical Data formatting to DF and csv save

--- data/data_retrieval.py
+++ data/data_retrieval.py
@@ -61,12 +61,12 @@ def convert_to_dataframe(time_series_data):
     return df
 
 
-def convert_econdata_to_dataframe(data_in,
-                                  function_name:str = ''):
+def convert_econdata_to_dataframe(data_in, function_name: str = ''):
     """
     Converts single econ_data format to dataframe:
 
     :param data_in: econ_data = output['CPI']['data']
+    :param function_name: string name of Economic Data.
     :return: df
     """
 
@@ -86,12 +86,17 @@ def convert_econdata_to_dataframe(data_in,
 
 
 def build_complete_econ_dataframe(all_econ_data):
+    """
+    Build a completely formatted economic data specific dataframe
+    :param all_econ_data: Dictionary of All Economic data obtained from the API Ping
+    :return: Dataframe of formatted economic data
+    """
     econ_functions = list(all_econ_data.keys())
     complete_df = None
     for x in econ_functions:
         df = convert_econdata_to_dataframe(all_econ_data[x]['data'],
                                            function_name=x)
-        if not isinstance(complete_df,pd.DataFrame):
+        if not isinstance(complete_df, pd.DataFrame):
             complete_df = df
         else:
             complete_df = complete_df.merge(df, on='timestamp', how='outer')
@@ -110,11 +115,49 @@ def build_complete_econ_dataframe(all_econ_data):
     return complete_df
 
 
-def save_dataframe_to_csv(df, filename):
-    df.to_csv(filename, index=False)
+def build_complete_technical_dataframe(all_technical_data):
+    """
+    create the complete, individual symbol, technical data
+    :param all_technical_data: all pricing and technical data, in dictionary format
+    :return: The complete formatted dataframe:
+    """
+    tech_functions = list(all_technical_data.keys())
+    complete_df = None
+    for x in tech_functions:
+        assert isinstance(all_technical_data[x], dict), (f'Datatype not expected. expected Dict,'
+                                                         f' got {type(all_technical_data[x])} ')
+        cur_keys = list(all_technical_data[x].keys())
+        # Expected keys ['Meta_Data', Function Name]
+        assert len(cur_keys) == 2, f'Expected number of keys is 2. Got {len(cur_keys)}'
+
+        df = convert_to_dataframe(all_technical_data[x][cur_keys[1]])
+
+        if not isinstance(complete_df, pd.DataFrame):
+            complete_df = df
+        else:
+            complete_df = complete_df.merge(df, on='timestamp', how='outer')
+            complete_df = complete_df.sort_values(by='timestamp').ffill()
+
+        for col in complete_df.columns:
+            if col != 'timestamp':
+                complete_df[col] = complete_df[col].astype(float)
+
+        columns_to_convert = list(complete_df.columns)
+        columns_to_convert.remove('timestamp')
+        existing_columns = [col for col in columns_to_convert if col in complete_df.columns]
+
+        # Ensures that the columns are numeric.
+        complete_df[existing_columns] = complete_df[existing_columns].apply(pd.to_numeric)
+
+    return complete_df
 
 
 def get_data(list_of_api_functions):
+    """
+    Function to acquire data via API ping
+    :param list_of_api_functions: lsit of api functions
+    :return: api information
+    """
     output_dict = {}
     for func in list_of_api_functions:
         if func['function'] == 'INFLATION':
@@ -134,6 +177,7 @@ def get_data(list_of_api_functions):
 
 if __name__ == '__main__':
     # Symbols and API Key
+    update_econ_data = False
 
     api_key = api_key.api_key
     symbol_dict = symbol_list.symbol_dict
@@ -155,10 +199,16 @@ if __name__ == '__main__':
 
                 input_list_core = [api_dict_WEEKLY_ADJUSTED]
                 output = get_data(input_list_core)
+
                 # Grabs Econ Data Once to prevent pinging API too many times.
-                if econ_data is None:
+                if update_econ_data:
                     econ_data = get_data(economic_data_functions)
-                    econ_data.to_csv('Economic_data_historical.csv')
+                    econ_data_formatted = build_complete_econ_dataframe(econ_data)
+                    econ_data_formatted.to_csv('Economic_data_historical.csv')
+                    # Remove data for memory efficiency
+                    del econ_data
+                    del econ_data_formatted
+                    update_econ_data = False
 
                 # Update Symbol based functions
                 for functions in function_groups:
@@ -175,5 +225,14 @@ if __name__ == '__main__':
                 ]
                 for indicators in all_tech_indicators:
                     output.update(get_data(indicators))
+                # Put a wait timer here. To prevent Overloading API.
+                # timer.pause()
+                # Depending on API Key level (max_pings per min)/60 = wait timer
 
+                completed_technical_df = build_complete_technical_dataframe(output)
+
+                # Add Sector column before saving
+
+
+                completed_technical_df.to_csv(f'{symbol}_Price_Technical')
                 print(f'Acquired information for {symbol} in {category} - {sector}')
